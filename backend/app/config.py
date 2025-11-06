23c57a91c216deb92510ba13e2556f8b7a2eee49
Anirudh Anilkumar <154657849+ANIRUD4@users.noreply.github.com>
2025-11-06 13:09:03 +0530

    restart

--- backend/app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-BASE_DIR = os.path.dirname(__file__)
-MODELS_DIR = os.path.join(BASE_DIR, 'models')
-DATASETS_DIR = os.path.join(BASE_DIR, 'datasets')
-OUTPUTS_DIR = os.path.join(BASE_DIR, 'outputs')
-BASE_YOLO_MODEL = os.path.join(MODELS_DIR, 'yolov8n.pt')
-CUSTOM_MODEL = os.path.join(MODELS_DIR, 'trained_custom.pt')
-DEFAULT_EPOCHS = 50
